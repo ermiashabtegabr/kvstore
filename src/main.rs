b4b3995b232ba56b2d60fc565bb0938300a70f9e
Ermias Habtegabr <habtegabrermias@gmail.com>
2023-05-26 14:40:25 +0200

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,51 @@
-pub mod kv;
-pub mod rest;
-pub mod rpc;
-pub mod server;
-pub mod util;
+mod grpc;
+mod kvstore;
 
-use omnipaxos::OmniPaxos;
-use omnipaxos_storage::memory_storage::MemoryStorage;
+use grpc::connection::proto;
+use serde::Deserialize;
+use tide::Request;
 
-type OmniPaxosKV<E> = OmniPaxos<E, MemoryStorage<E>>;
+#[derive(Clone, Debug, Deserialize)]
+struct Get {
+    key: String,
+}
 
-fn main() {
-    unimplemented!();
+#[derive(Clone, Debug, Deserialize)]
+struct Set {
+    key: String,
+    value: u64,
+}
+
+fn _node_rpc_addr(id: usize) -> String {
+    // Get the id of the node
+    // Set the rpc address to <name>-<id>.<serviceName>
+    format!("{}.serviceName", id)
+}
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    println!("[INFO] listening on 0.0.0.0:8080");
+
+    let mut server = tide::new();
+    server.at("/").get(|_| async { Ok("PING") });
+    server.at("/set").post(set_value);
+    server.at("/get").get(get_value);
+    server.listen("0.0.0.0:8080").await?;
+
+    Ok(())
+}
+
+async fn set_value(mut req: Request<()>) -> tide::Result {
+    let Set { key, value } = req.body_json().await?;
+    println!("{}:{}", key, value);
+    Ok(format!("{} -> {}", key, value).into())
+}
+
+async fn get_value(req: Request<()>) -> tide::Result {
+    let get_request: Get = req.query().expect("Failed to parde query");
+    let _get_request_proto = proto::Get {
+        key: get_request.key,
+    };
+
+    Ok(format!("huh").into())
 }
--- src/rpc/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod client;
-pub mod connection;
-pub mod parse_utils;
-pub mod server;
